ba270f484d3a5f6900cb47183d8bee3f1ef97d60
brendanthebigboi <brendansides12@gmail.com>
2024-03-04 01:19:47 -0600

    dshb Add assets

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="coming-soon">
 
-      <Image src="https://cdn.sideforge.io/sentinel/Logo_Single_1.png" height="100" width="100" alt="Logo"/>
+      <Image src="https://cdn.sideforge.io/Sentinel/Primary Logomark - Housed.png" height="100" width="100" alt="Logo"/>
 
       <h1>Coming Soon!</h1>
       <p>This product is coming soon, thanks for your patience.</p>
