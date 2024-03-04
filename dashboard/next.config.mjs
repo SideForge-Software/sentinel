dde2e660a9616b12b10d844da0525fb215eda465
brendanthebigboi <brendansides12@gmail.com>
2024-03-04 00:51:12 -0600

    dshb Add image domain conf and coming soon sec

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        // Allow all of the image cdn domains, such as cdn.dsicordapp or cdn.sideforge
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
